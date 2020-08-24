a408e1630affb57f28f674b386667c9ee6491996
Ly Chan <chan.ly@boot.ai>
2020-08-24 23:15:24 +0700

    add: Interlock giữa các functions ở backend

---
+++ main2.py
@@ -0,0 +1,26 @@
+import json
+
+import pandas as pd
+
+XLSX_NAME = '../HTC_EN_Locking list (Chapter 7)_v1_28042020.xlsx'
+JSON_NAME = 'output_data.json'
+
+xlsx = pd.read_excel(XLSX_NAME, None)
+
+data = []
+
+df = pd.DataFrame(xlsx["Sheet1"])
+
+for i in range(0, 25):
+    list_disable = []
+    for j in range(1, 26):
+        if df.loc[0][j] == "x":
+            list_disable.append(df.columns[j])
+    function = {
+        df.loc[i][0]: list_disable
+    }
+    data.append(function)
+
+with open(JSON_NAME, 'w') as file:
+    json.dump(data, file, indent=4)
+
